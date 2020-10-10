4ed8ef6786c29720cbdb2a76004667fe39df6720
Thiago <thiagolima1011@gmail.com>
2020-10-10 12:55:00 -0300

    Adicionado mais um arquivo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ExerciciosAulaJs
+# Projeto curso algoritmo I
+## Esse projeto é sobre um curso de algoritmo e lógica 
---
+++ operadores1.js
@@ -0,0 +1,7 @@
+let resultado, valor01, valor02, valor03
+valor01 = 10
+valor02 = 10
+valor03 = 10
+resultado = ((valor01 + valor02) * valor03)
+alert("Resultado: " +resultado)
+console.log(resultado)
